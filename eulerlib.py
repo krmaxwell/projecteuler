b0bdf6f9b4e4cb1c55223ddf77e5cb1873473af8
Kyle Maxwell <kylem@xwell.org>
2012-04-29 11:26:22 -0500

    Started library of functions

---
+++ eulerlib.py
@@ -0,0 +1,20 @@
+# Frequently-used routines for Project Euler
+
+def prime_factors(n):
+    """ Return the prime factors of the given number. """
+    factors = []
+    lastresult = n
+
+    # 1 is a special case
+    if n == 1:
+        return [1]
+
+    while lastresult != 1:
+        c = 2
+        while lastresult % c != 0:
+            c += 1
+        factors.append(c)
+        lastresult /= c
+
+    return factors
+
