ee66fb8c34d2888a0da0e4e46e5761674eb38e48
Kyle Maxwell <kylem@xwell.org>
2012-04-29 12:09:23 -0500

    Refactored prob3.py for new version of prime_factors

--- prob3.py
+++ prob3.py
@@ -4,4 +4,15 @@ import sys
 from eulerlib import *
 
 # All the work is done by the module
-print prime_factors(600851475143)
+n= 600851475143
+factors = prime_factors(n)
+print "Factoring",n,"into:"
+first = True
+for n in factors:
+	if not first:
+		print "*",
+	print n,
+	if factors[n] != 1:
+		print "^",
+		print factors[n],
+	first = False
