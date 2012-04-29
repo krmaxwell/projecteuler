589b7f230bfffd0cbe5cbb1f1128389f847dbe4b
Kyle Maxwell <kylem@xwell.org>
2012-04-29 11:47:31 -0500

    Comment explaining why this doesn't work

--- prob5.py
+++ prob5.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python
 
+from eulerlib import *
+
 max=10 
 lcm=max
 
-def prime_factors(n):
-    """ Return the prime factors of the given number. """
-    factors = []
-    lastresult = n
-
-    # 1 is a special case
-    if n == 1:
-        return [1]
-
-    while lastresult != 1:
-        c = 2
-        while lastresult % c != 0:
-            c += 1
-        factors.append(c)
-        lastresult /= c
-
-    return factors
+""" 
+The following method uses prime factorization and isn't working. This is 
+because we don't account for prime factors raised to a power, e.g. 8=2^3.
+"""
 
 for i in range(max-1,2,-1):
 	# if the lcm candidate is already divisible, we're done with that one
 	print "Examining ",i," for ",lcm
 	if (lcm % i) != 0:
+		factors=prime_factors(i)
+		print "* Factors for",i,"are",factors # debug output, really
 		for j in prime_factors(i):
-			if (lcm % j != 0):
-				print "  Multiplying by ",j
+			if (lcm % j != 0): # WRONG
+				print "* Multiplying by ",j
 				lcm *= j
 	else:
 		print i," is already a factor"
