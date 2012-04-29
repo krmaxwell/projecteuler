1fdaa8deb7919c6146c4b7a810be6b7b91aa2356
Kyle Maxwell <kylem@xwell.org>
2012-04-29 12:40:11 -0500

    Solved problem 5\!

--- prob5.py
+++ prob5.py
@@ -2,25 +2,20 @@
 
 from eulerlib import *
 
-max=10 
-lcm=max
+max=20 
+lcm=1
+factors={}
 
-""" 
-The following method uses prime factorization and isn't working. This is 
-because we don't account for prime factors raised to a power, e.g. 8=2^3.
-"""
-
-for i in range(max-1,2,-1):
-	# if the lcm candidate is already divisible, we're done with that one
-	print "Examining ",i," for ",lcm
-	if (lcm % i) != 0:
-		factors=prime_factors(i)
-		print "* Factors for",i,"are",factors # debug output, really
-		for j in prime_factors(i):
-			if (lcm % j != 0): # WRONG
-				print "* Multiplying by ",j
-				lcm *= j
-	else:
-		print i," is already a factor"
+for i in range(max-1,1,-1):
+    candidate_factors=prime_factors(i)
+    print "* Factors for",i,"are: ",
+    print_factors(candidate_factors)
+    for j in candidate_factors:
+        if (not j in factors) or (candidate_factors[j] > factors[j]):
+	    factors[j]=candidate_factors[j]
 	
+print "Factors of LCM are",
+print_factors(factors)
+for f in factors:
+    lcm = lcm * f ** factors[f]
 print "LCM is",lcm
