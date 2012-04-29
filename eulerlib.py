94ab20658dcaf4240198b80ef7791a4316fd2d17
Kyle Maxwell <kylem@xwell.org>
2012-04-29 11:56:34 -0500

    Refactored prime_factors to return a dict

--- eulerlib.py
+++ eulerlib.py
@@ -2,18 +2,21 @@
 
 def prime_factors(n):
     """ Return the prime factors of the given number. """
-    factors = []
+    factors = {}
     lastresult = n
 
     # 1 is a special case
     if n == 1:
-        return [1]
+        return {1: 1}
 
     while lastresult != 1:
         c = 2
         while lastresult % c != 0:
             c += 1
-        factors.append(c)
+	if c in factors:
+	    factors[c] += 1
+	else:
+	    factors[c] = 1
         lastresult /= c
 
     return factors
