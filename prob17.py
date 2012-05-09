f22995c8a24d5ae39493ed8410888c627a59d695
Kyle Maxwell <kylem@xwell.org>
2012-05-08 21:53:23 -0500

    Basic logic in place, some corner cases not right

--- prob17.py
+++ prob17.py
@@ -2,7 +2,31 @@
 
 import sys
 
+ones = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+flag = False
+numstr = ''
+
 if len(sys.argv) == 1:
     print "Give me a number"
+    flag = True
 else:
     n = int(sys.argv[1])
+    if n >= 1000:
+	print "Number should be under 1000"
+	flag = True
+    if n <= 0:
+	print "Number should be positive"
+        flag = True
+
+if not flag:
+    hundred = n/100
+    n = n % 100
+    ten = n/10
+    n = n % 10
+    print hundred,' ',ten,' ',n
+    if hundred > 1:
+	print ones[hundred-1],"hundred and"
+    if ten > 2:
+	print tens[ten-1]
+    print ones[n-1]
