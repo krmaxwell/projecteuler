a7499fe28b904ed3a2ed911ea74945e28aeca90e
Kyle Maxwell <kylem@xwell.org>
2013-12-26 11:06:55 -0600

    Modified problem 3 just to show the actual answer instead of all factors

--- prob3.py
+++ prob3.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python
-
-import sys
 from eulerlib import *
 
 # All the work is done by the module
 n= 600851475143
 factors = prime_factors(n)
-print n," factors into:"
-print_factors(factors)
+print "Largest prime factor of %d is %d" % (n, max(factors.keys()))
