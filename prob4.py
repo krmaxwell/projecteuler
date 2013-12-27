eea0c837ff8d80c8e5d87f55177bcfc66879496e
Kyle Maxwell <kylem@xwell.org>
2013-12-26 20:47:32 -0600

    PEP8 compliance because I am that sort of nerd

--- prob4.py
+++ prob4.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python
-
 from eulerlib import *
 
-result=0
-for i in range(100,1000):
-	for j in range (100,1000):
-		n=i*j
-		if (is_palindrome(n)) and n>result:
-			lasti = i
-			lastj = j
-			result = n
-print result," is the product of ",lasti," and ",lastj
+result = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        n = i*j
+        if (is_palindrome(n)) and n > result:
+            lasti = i
+            lastj = j
+            result = n
+print "%d is the product of %d and %d" % (result, lasti, lastj)
