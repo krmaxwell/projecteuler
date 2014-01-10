87610bb091dc35b79692642efcaf589569a1c64c
Kyle Maxwell <kylem@xwell.org>
2014-01-09 21:11:30 -0600

    Readability fixes and allow command-line parameter for max in set

--- prob5.py
+++ prob5.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
+import sys
+
 from eulerlib import *
 
-max=20 
+if len(sys.argv) == 1:
+    n = 10
+else:
+    n = sys.argv[1]
+
 lcm=1
 factors={}
 
-for i in range(max-1,1,-1):
-    candidate_factors=prime_factors(i)
-    print "* Factors for",i,"are: ",
-    print_factors(candidate_factors)
+for i in range(n-1, 1, -1):
+    candidate_factors = prime_factors(i)
+    print "* Factors for %d are: %s" % (i, print_factors(candidate_factors))
     for j in candidate_factors:
         if (not j in factors) or (candidate_factors[j] > factors[j]):
-	    factors[j]=candidate_factors[j]
-	
-print "Factors of LCM are",
-print_factors(factors)
+            factors[j] = candidate_factors[j]
+
+print "Factors of LCM are %s" % print_factors(factors)
 for f in factors:
     lcm = lcm * f ** factors[f]
-print "LCM is",lcm
+print "LCM is %d" %lcm
